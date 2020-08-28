437ff5046591706922fc78e0b84623b4e5bafe75
Nathan Hensel <nathanihensel@gmail.com>
2020-08-28 03:48:20 +0000

    split data into canonical schemas

--- src/make_i2b2.sh
+++ src/make_i2b2.sh
@@ -11,18 +11,19 @@ fi
 
 homedir="$(pwd)"
 
+# db.properties template sans jdbc string
 cat > db.properties << EOF
 db.type=postgresql
 db.username=i2b2
 db.password=${TF_VAR_I2B2_DB_PASS}
 db.driver=org.postgresql.Driver
-db.url=jdbc:postgresql://postgres-0:5432/i2b2?currentSchema=crc
 db.project=demo
 EOF
 
 # load crc
 cd "${homedir}"/i2b2-data/edu.harvard.i2b2.data/Release_1-7/NewInstall/Crcdata
 cp "${homedir}"/db.properties .
+echo "db.url=jdbc:postgresql://postgres-0:5432/i2b2?currentSchema=crc" >> db.properties
 ant -f data_build.xml create_crcdata_tables_release_1-7
 ant -f data_build.xml create_procedures_release_1-7
 ant -f data_build.xml db_demodata_load_data
@@ -30,6 +31,7 @@ ant -f data_build.xml db_demodata_load_data
 # load hive
 cd "${homedir}"/i2b2-data/edu.harvard.i2b2.data/Release_1-7/NewInstall/Hivedata
 cp "${homedir}"/db.properties .
+echo "db.url=jdbc:postgresql://postgres-0:5432/i2b2?currentSchema=hive" >> db.properties
 ant -f data_build.xml create_hivedata_tables_release_1-7
 ant -f data_build.xml db_hivedata_load_data
 
@@ -40,7 +42,7 @@ db.type=postgresql
 db.username=i2b2
 db.password=${TF_VAR_I2B2_DB_PASS}
 db.driver=org.postgresql.Driver
-db.url=jdbc:postgresql://postgres-0:5432/i2b2?currentSchema=crc
+db.url=jdbc:postgresql://postgres-0:5432/i2b2?currentSchema=metadata
 db.project=demo
 db.dimension=OBSERVATION_FACT
 db.schemaname=I2B2DEMODATA
@@ -54,6 +56,7 @@ ant -f data_build.xml db_metadata_load_data
 # load pmdata
 cd "${homedir}"/i2b2-data/edu.harvard.i2b2.data/Release_1-7/NewInstall/Pmdata
 cp "${homedir}"/db.properties .
+echo "db.url=jdbc:postgresql://postgres-0:5432/i2b2?currentSchema=pm" >> db.properties
 ant -f data_build.xml create_pmdata_tables_release_1-7
 ant -f data_build.xml create_triggers_release_1-7
 ant -f data_build.xml db_pmdata_load_data
@@ -61,5 +64,6 @@ ant -f data_build.xml db_pmdata_load_data
 # load workdata
 cd "${homedir}"/i2b2-data/edu.harvard.i2b2.data/Release_1-7/NewInstall/Workdata
 cp "${homedir}"/db.properties .
+echo "db.url=jdbc:postgresql://postgres-0:5432/i2b2?currentSchema=workdata" >> db.properties
 ant -f data_build.xml create_workdata_tables_release_1-7
 ant -f data_build.xml db_workdata_load_data
