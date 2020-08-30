71445a1f0a4572d31af468172e6a26f5c5538983
Nathan Hensel <nathanihensel@gmail.com>
2020-08-30 20:23:04 +0000

    gz extension

--- src/make_db-artifact.sh
+++ src/make_db-artifact.sh
@@ -4,4 +4,4 @@ set -euxo pipefail
 hash pg_dump pigz
 
 export PGPASSWORD="$TF_VAR_I2B2_DB_PASS"
-pg_dump -h "$DB_HOST" -U i2b2 i2b2 | pigz -9 > i2b2.sql
+pg_dump -h "$DB_HOST" -U i2b2 i2b2 | pigz -9 > i2b2.sql.gz
